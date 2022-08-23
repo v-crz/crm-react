2abc2031263a03c4206e30e618fac659a3d9f119
viry <vbatten10@hotmail.com>
2022-08-23 12:53:47 -0500

    Agrupando rutas y definiendo layout principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import IniciarSesion from './layout/IniciarSesion';
 import Layout from './layout/Layout';
 import Inicio from './paginas/Inicio';
-import LoginForm from './paginas/LoginForm';
+import NuevoCliente from './paginas/NuevoCliente';
+import EditarCliente from './paginas/EditarCliente';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IniciarSesion />}>
-          <Route index element={<LoginForm />} />
-        </Route>
-
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
+          <Route path="nuevo" element={<NuevoCliente />} />
+          <Route path="editar/:id" element={<EditarCliente />} />
         </Route>
       </Routes>
     </BrowserRouter>
